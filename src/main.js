d67a2e57219d64a3404ea33ac1787324c187bea4
Simeon Rediger <simeonrediger@gmail.com>
2025-12-28 17:41:25 -0500

    Replace shortest-path.js with pathfinder.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import board from './board.js';
-import getShortestPath from './shortest-path.js';
 import knight from './knight.js';
+import pathfinder from './pathfinder.js';
 
 console.clear();
 
 const start = [0, 0];
 const end = [1, 2];
-const path = getShortestPath(board, knight, start, end);
+const path = pathfinder.getShortestPath(board, knight, start, end);
 
 console.log('Path:', path);
 console.log('Length:', path?.length);
