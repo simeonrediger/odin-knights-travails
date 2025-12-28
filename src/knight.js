18dc0be74858f9735ebb811a3f12f04b4d435a84
Simeon Rediger <simeonrediger@gmail.com>
2025-12-27 22:09:05 -0500

    Add moveset

--- src/knight.js
+++ src/knight.js
@@ -1,9 +1,21 @@
 let rank;
 let file;
 
+const moveset = Object.freeze([
+    [1, 2],
+    [2, 1],
+    [2, -1],
+    [1, -2],
+    [-1, -2],
+    [-2, -1],
+    [-2, 1],
+    [-1, 2],
+]);
+
 const knight = {
     rank,
     file,
+    moveset,
 };
 
 export default knight;
