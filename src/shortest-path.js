6bc47132b19c1265f0cef47f85751e3de9c0cd0e
Simeon Rediger <simeonrediger@gmail.com>
2025-12-27 22:11:27 -0500

    Revert destructuring of start and end parameters

--- src/shortest-path.js
+++ src/shortest-path.js
@@ -1,10 +1,5 @@
-export default function getShortestPath(
-    board,
-    knight,
-    [startRank, startFile],
-    [endRank, endFile],
-) {
-    if (!board.contains([startRank, startFile], [endRank, endFile])) {
+export default function getShortestPath(board, knight, start, end) {
+    if (!board.contains(start, end)) {
         throw new RangeError(
             'Start and end ranks and files must be within [0, 8)',
         );
