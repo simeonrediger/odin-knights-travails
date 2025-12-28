575e789ce1d35ffa92a3ceada45468e9b35049c4
Simeon Rediger <simeonrediger@gmail.com>
2025-12-28 17:30:00 -0500

    Add applyMove()

--- src/knight.js
+++ src/knight.js
@@ -11,12 +11,18 @@ const moveset = Object.freeze([
     [-1, 2],
 ]);
 
+function applyMove(move) {
+    knight.position[0] += move.position[0];
+    knight.position[1] += move.position[1];
+}
+
 function computePosition(move) {
     return [knight.position[0] + move[0], knight.position[1] + move[1]];
 }
 
 const knight = {
     moveset,
+    applyMove,
     computePosition,
 
     get position() {
