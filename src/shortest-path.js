61c0997804cc361098d95bda62fa3d44ad61efd8
Simeon Rediger <simeonrediger@gmail.com>
2025-12-28 13:39:17 -0500

    Get unique possible moves

--- src/shortest-path.js
+++ src/shortest-path.js
@@ -14,4 +14,32 @@ function validatePositions(board, ...positions) {
 
 function pathfind(board, knight, start, end, visited = []) {
     visited.push(knight.position);
+    const uniquePossibleMoves = getUniquePossibleMoves(board, knight, visited);
+}
+
+function getUniquePossibleMoves(board, knight, visited) {
+    return knight.moveset.filter(move => {
+        const newPosition = [
+            knight.position[0] + move[0],
+            knight.position[1] + move[1],
+        ];
+
+        const possible =
+            newPosition[0] >= 0 &&
+            newPosition[0] < board.size &&
+            newPosition[1] >= 0 &&
+            newPosition[1] < board.size;
+
+        if (!possible) {
+            return false;
+        }
+
+        const alreadyVisited = visited.some(
+            visitedPosition =>
+                newPosition[0] === visitedPosition[0] &&
+                newPosition[1] === visitedPosition[1],
+        );
+
+        return !alreadyVisited;
+    });
 }
