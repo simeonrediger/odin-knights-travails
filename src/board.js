4d99eccb1f8a88e9b862d70bc7b36ad457a0d836
Simeon Rediger <simeonrediger@gmail.com>
2025-12-27 22:01:59 -0500

    Add contains()

--- src/board.js
+++ src/board.js
@@ -1,3 +1,13 @@
-const board = {};
+const size = 8;
+
+function contains(...positions) {
+    return positions.every(
+        ([rank, file]) => rank >= 0 && rank < size && file >= 0 && file < size,
+    );
+}
+
+const board = {
+    contains,
+};
 
 export default board;
