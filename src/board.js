10cc7951aaae3ed92297d1da569d615d77d5dd61
Simeon Rediger <simeonrediger@gmail.com>
2025-12-29 15:54:27 -0500

    Convert contains() to use x and y args

--- src/board.js
+++ src/board.js
@@ -1,9 +1,7 @@
 const size = 8;
 
-function contains(...positions) {
-    return positions.every(
-        ([x, y]) => x >= 0 && x < size && y >= 0 && y < size,
-    );
+function contains(x, y) {
+    return x >= 0 && x < size && y >= 0 && y < size;
 }
 
 function sameSquare(x1, y1, x2, y2) {
