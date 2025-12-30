e6f1d80780cb60885c749b87db265b405d038b3b
Simeon Rediger <simeonrediger@gmail.com>
2025-12-29 21:19:03 -0500

    Remove console.clear()

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import board from './board.js';
 import knight from './knight.js';
 import pathfinder from './pathfinder.js';
 
-console.clear();
-
 const start = { x: 0, y: 0 };
 const end = { x: 7, y: 7 };
 
